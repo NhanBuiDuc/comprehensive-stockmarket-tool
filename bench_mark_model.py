7160bd237fbcab8d49a82335f82a671b3302f73b
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2023-04-13 17:18:28 +0700

    update config

--- bench_mark_model.py
+++ bench_mark_model.py
@@ -10,8 +10,22 @@ from keras.layers import LSTM, Dense, Dropout
 from config import config as cf
 from keras.optimizers import Adam
 import numpy as np
-def bench_mark_random_forest(dataset_train, dataset_val, dataset_test):
-    pass
+from sklearn.ensemble import RandomForestRegressor
+
+def bench_mark_random_forest(X_train, y_train, X_val, y_val, X_test, y_test):
+
+
+    # create random forest regressor object
+    rf = RandomForestRegressor()
+
+    # train the regressor using the training data
+    rf.fit(X_train, y_train)
+
+    # evaluate the regressor on the validation data
+    val_error = mean_squared_error(y_val, rf.predict(X_val))
+
+    # evaluate the regressor on the test data
+    test_error = mean_squared_error(y_test, rf.predict(X_test))
 
 def create_lstm_model(X_train, y_train, X_val, y_val, X_test, y_test):
     model = Sequential()
@@ -31,5 +45,4 @@ def create_lstm_model(X_train, y_train, X_val, y_val, X_test, y_test):
 
     history_LSTM = model.fit(X_train, y_train, epochs=100, batch_size=cf['training']['movement_3']['batch_size'], validation_data=(X_val, y_val))
     loss, _ = model.evaluate(X_test, y_test)
-    print('Test Loss MSE: ', loss)
     return model, history_LSTM, loss
