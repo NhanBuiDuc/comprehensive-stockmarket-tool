9b52f02c48bf5f11c904accc5e787c1036892594
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2023-04-14 23:35:43 +0700

    update

--- dev.py
+++ dev.py
@@ -528,38 +528,3 @@ if __name__ == "__main__":
                     train_df, valid_df,
                     test_df, train_date,valid_date, test_date,
                     data_dates, show_heat_map = False, is_train = True)
-    # train_movement_3(data_df, 
-    #                 num_data_points,
-    #                 train_df, valid_df,
-    #                 test_df, train_date,valid_date, test_date,
-    #                 data_dates, show_heat_map = False, is_train = True)
-                    
-    # train_magnitude_3(data_df, 
-    #                 num_data_points,
-    #                 train_df, valid_df,
-    #                 test_df, train_date,valid_date, test_date,
-    #                 data_dates, show_heat_map = False, is_train = True)
-
-    # train_movement_7(data_df, 
-    #                 num_data_points,
-    #                 train_df, valid_df,
-    #                 test_df, train_date,valid_date, test_date,
-    #                 data_dates, show_heat_map = False, is_train = True)
-                    
-    # train_magnitude_7(data_df, 
-    #                 num_data_points,
-    #                 train_df, valid_df,
-    #                 test_df, train_date,valid_date, test_date,
-    #                 data_dates, show_heat_map = False, is_train = True)
-
-    # train_movement_14(data_df, 
-    #                 num_data_points,
-    #                 train_df, valid_df,
-    #                 test_df, train_date,valid_date, test_date,
-    #                 data_dates, show_heat_map = False, is_train = True)
-                    
-    # train_magnitude_14(data_df, 
-    #                 num_data_points,
-    #                 train_df, valid_df,
-    #                 test_df, train_date,valid_date, test_date,
-    #                 data_dates, show_heat_map = False, is_train = True)
