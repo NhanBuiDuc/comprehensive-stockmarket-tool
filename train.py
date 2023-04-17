91cb4107a24ebafcf03eac8953bea848dffe4267
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2023-04-18 04:48:03 +0700

    update eval

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from model import Model
 trainer = Trainer()
 model_name = "movement_1"
 file_name = "AAPL_movement_1.pth"
-model = trainer.train(model_name, new_data=True)
+# model = trainer.train(model_name, new_data=True)
 model = Model(name=model_name)
 model = model.load_check_point(file_name)
 trainer.eval(model)
