2489ce6f2a9b875879c1298355085aaaf69fec3d
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2023-04-18 12:07:28 +0700

    update eval

--- trainer.py
+++ trainer.py
@@ -73,9 +73,9 @@ class Trainer:
             criterion = nn.BCELoss()
 
         if "adam" in optimizer:
-            optimizer = optim.Adam(model.structure.parameters(), lr=learning_rate, weight_decay=0.001)
+            optimizer = optim.Adam(model.structure.parameters(), lr=learning_rate, weight_decay=0.1)
         elif "sgd" in optimizer:
-            optimizer = optim.SGD(model.structure.parameters(), lr=learning_rate, weight_decay=0.001)
+            optimizer = optim.SGD(model.structure.parameters(), lr=learning_rate, weight_decay=0.1)
         scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1,
                                       patience=scheduler_step_size, verbose=True)
 
