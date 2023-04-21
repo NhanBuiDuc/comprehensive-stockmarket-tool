5ec695442b8562824efd880869bc96f732eb361c
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2023-04-21 18:22:41 +0700

    more

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ trainer = Trainer()
 tf_trainer = TensorflowTrainer()
 model_name = "movement_1"
 file_name = "AAPL_movement_1.pth"
-model = trainer.train(model_name, new_data=True)
+model = trainer.train(model_name, new_data=True, full_data = False)
 # tf_trainer.train("svm_1", new_data=True)
 model = Model(name=model_name)
 model = model.load_check_point(file_name)
