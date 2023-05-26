4fa786a6bf2d9c8041ccf8ecd41baca3fb0b2f5a
anhkhoanghg <77378155+anhkhoanghg@users.noreply.github.com>
2023-05-26 14:29:22 +0700

    dataset aapl

--- configs/transformer_cf.py
+++ configs/transformer_cf.py
@@ -1,17 +1,17 @@
 transformer_cf = {
     "model": {
         "nhead": 3,
-        "num_encoder_layers": 10,
-        "dim_feedforward": 10,
+        "num_encoder_layers": 40,
+        "dim_feedforward": 40,
         "dropout": 0.3,
-        "window_size": 3,
+        "window_size": 14,
         "output_step": 3,
         "topk": 5
     },
     "training": {
         "device": "cuda",  # "cuda" or "cpu"
         "batch_size": 64,
-        "num_epoch": 1,
+        "num_epoch": 200,
         "learning_rate": 0.001,
         "loss": "focal",
         "evaluate": ["bce", "accuracy", "precision", "f1"],
